1216b16971ffe86a9f7d918ba1c6fd533d68135b
elvin <jonelvinnaorbe@gmail.com>
2018-03-16 23:31:16 +0800

    Add 3d and responsive

--- kjus/app/scripts/main.js
+++ kjus/app/scripts/main.js
@@ -25,7 +25,59 @@ $(window).load(function(){
       }
     });
 
-    jQuery('.menu-toggle').click(function(){
-        $("#main-nav").slideToggle();
+    $('.menu-toggle').click(function(){
+        $(".main-menu").slideToggle();
+    });
+
+    $('.product-info, .holder-3d__point--1').connections();
+    //$('.holder-3d__point--2,.product-info').connections();
+
+    $('.holder-3d__point').click(function(){
+      var content = $(this).find('.holder-3d__text').html();
+      var id = $(this).attr('id');
+      $('.product-info').html('');
+      $('.product-info').html(content);
+      $('.product-info').connections('remove');
+      $('#' + id + ',.product-info').connections();
+    });
+
+    var isDragging = false;
+    $("#image-reel")
+    .mousedown(function() {
+        isDragging = false;
+    })
+    .mousemove(function() {
+        isDragging = true;
+     })
+    .mouseup(function() {
+        var wasDragging = isDragging;
+        isDragging = false;
+        var original = $('#image-reel img').attr('data-original');
+        var img = $('#image-reel img').attr('src');
+        
+
+
+        if (!wasDragging) {
+          //not dragging
+          if (img.indexOf(original) > -1)
+          {
+            $('.holder-3d__point').show();
+            //$('.product-info').connections('remove');
+          }else{
+            $('.holder-3d__point').hide();
+            $('.product-info').connections('remove');
+          }
+          
+        }else{
+          //dragging
+          if (img.indexOf(original) > -1)
+          {
+            $('.holder-3d__point').show();
+            //$('.product-info').connections('remove');
+          }else{
+            $('.holder-3d__point').hide();
+            $('.product-info').connections('remove');
+          }
+        }
     });
 });
